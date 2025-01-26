b00fb16704c8d14806d955efbb27fd8c672c2e37
aedev <elbedawia@yahoo.com>
2025-01-25 20:08:09 -0500

    Device rotation triggers on right-click

--- Assets/Scripts/DeviceController.cs
+++ Assets/Scripts/DeviceController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 public class DeviceController : MonoBehaviour
 {
     public float rotationSpeed;
+    public float resetSpeed = 10f;
 
     private float X;
     private float Y;
+
+    private float lerpUnit = 0.01f;
 
     // Update is called once per frame
     void Update()
     {
         
-
+        // Rotate device on right-click.
         if (Input.GetMouseButton(1))
         {
             transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * rotationSpeed, -Input.GetAxis("Mouse X") * rotationSpeed, 0));
@@ -22,11 +25,10 @@ public class DeviceController : MonoBehaviour
         }
         else
         {
-
+            // Reset rotation when right-click is released.
             if (transform.rotation != Quaternion.Euler(0, 0, 0))
             {
-                Debug.Log(transform.rotation.eulerAngles);
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), 0.1f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), lerpUnit * resetSpeed);
             }
         }
     }
